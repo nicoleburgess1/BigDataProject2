48274cfd54fca995e9b80c361c078b905164af22
nicoleburgess1 <nburgess1113@gmail.com>
2024-10-03 15:20:11 -0400

    mapper1 done

--- src/test/java/Task1.java
+++ src/test/java/Task1.java
@@ -1,2 +1,103 @@
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import java.util.Random;
+
 public class Task1 {
+
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, Text>{
+
+        private final static IntWritable one = new IntWritable(1);
+        //private Text Education = new Text();
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            Random rand = new Random();
+            //StringTokenizer itr = new StringTokenizer(value.toString());
+            String dataset = value.toString();
+            String[] datapoint = dataset.split("/n");
+            String[][] columns = new String[datapoint.length][];
+            int[][] points = new int[datapoint.length][];
+            for(int i = 0; i < columns.length; i++){
+                columns[i] = datapoint[i].split(",");
+                for(int j = 0; j < columns[i].length; j++){
+                    points[i][j] = Integer.parseInt(columns[i][j]);
+                }
+            }
+
+            int[][] centers = new int[5][];
+            for(int i = 0; i < centers.length; i++){
+                centers[i] = points[rand.nextInt(points.length)];
+            }
+
+            for(int i = 0; i < points.length; i++){
+                int minDistance = -1;
+                int[] closestCenter = null;
+                for(int j = 0; j < centers[i].length; j++){
+                    int distance = euclideanDistance.euclidianDistance(points[i], centers[j]);
+                    if(minDistance == -1 || distance < minDistance){
+                        closestCenter = centers[j];
+                        minDistance = distance;
+                    }
+                }
+                context.write(convertPointToText(points[i]), convertPointToText(closestCenter));
+            }
+        }
+        public Text convertPointToText(int[] point){
+            return new Text(point[0]+","+point[1]+","+point[2]);
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public void debug(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Education Count");
+        job.setJarByClass(Task1.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Education Count");
+        job.setJarByClass(Task1.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path("input/LinkBookPage.csv"));
+        FileOutputFormat.setOutputPath(job, new Path("TaskAOutput"));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
 }
--- src/test/java/euclideanDistance.java
+++ src/test/java/euclideanDistance.java
@@ -2,7 +2,13 @@ import java.lang.Math;
 
 public class euclideanDistance {
 
-    public static double euclidianDistance (int x1, int y1, int z1, int x2, int y2, int z2) {
-        return java.lang.Math.sqrt((java.lang.Math.pow(x2-x1, 2)) + (java.lang.Math.pow(y2-y1, 2)) +(java.lang.Math.pow(z2-z1, 2)));
+    public static int euclidianDistance (int[] point1,int[] point2) {
+        int x1 = point1[0];
+        int y1 = point1[1];
+        int z1 = point1[2];
+        int x2 = point2[0];
+        int y2 = point2[1];
+        int z2 = point2[2];
+        return (int) Math.round(java.lang.Math.sqrt((java.lang.Math.pow(x2-x1, 2)) + (java.lang.Math.pow(y2-y1, 2)) +(java.lang.Math.pow(z2-z1, 2))));
     }
 }
