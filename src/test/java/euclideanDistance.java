e730de5d086525e8017e01fedb98309b4d39d0f0
aalapsley <114630770+aalapsley1@users.noreply.github.com>
2024-10-03 14:32:48 -0400

    euclidean distance

---
+++ src/test/java/euclideanDistance.java
@@ -0,0 +1,8 @@
+import java.lang.Math;
+
+public class euclideanDistance {
+
+    public static double euclidianDistance (int x1, int y1, int z1, int x2, int y2, int z2) {
+        return java.lang.Math.sqrt((java.lang.Math.pow(x2-x1, 2)) + (java.lang.Math.pow(y2-y1, 2)) +(java.lang.Math.pow(z2-z1, 2)));
+    }
+}
