4d1b735d3a9026267f369d97bf81efd9e1989d8e
aalapsley <114630770+aalapsley1@users.noreply.github.com>
2024-10-03 14:02:26 -0400

    create data

--- src/test/java/createData.java
+++ src/test/java/createData.java
@@ -1,5 +1,29 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Random;
+
+
 public class createData {
+
+    public static void createClustering() {
+        Random rand = new Random();
+        File data = new File("clustering.csv");
+        try (PrintWriter writer = new PrintWriter(new FileWriter(data))) {
+            for(int i=1; i<=5000; i++){
+                int x = rand.nextInt(10000);
+                int y = rand.nextInt(10000);
+                int z = rand.nextInt(10000);
+                writer.println( x + "," + y + "," + z);
+            }
+            System.out.println("CSV file written successfully.");
+        } catch (IOException e) {
+            System.out.println("An error occurred while writing the CSV file.");
+        }
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        createClustering();
     }
 }
